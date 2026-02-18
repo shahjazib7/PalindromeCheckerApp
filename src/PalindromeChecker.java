e6f54ec1a51e40a8afab596ff4236486ae7197be
shahjazib7 <sz9838@srmist.edu.in>
2026-02-18 10:15:38 +0530

    UC-4

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -4,28 +4,33 @@ public class PalindromeChecker {
     public static void main(String[] args) {
 
 
-        String input = "madam";
-        String str = input.toLowerCase();
+                String input = "madam";
 
-        int left = 0;
-        int right = str.length() - 1;
-        boolean isPalindrome = true;
+                input = input.toLowerCase();
 
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                isPalindrome = false;
-                break;
+                Stack<Character> stack = new Stack<>();
+
+
+                for (int i = 0; i < input.length(); i++) {
+                    stack.push(input.charAt(i));
+                }
+
+
+                String reversed = "";
+
+                while (!stack.isEmpty()) {
+                    reversed += stack.pop();
+                }
+
+
+                System.out.println("Original String: " + input);
+                if (input.equals(reversed)) {
+                    System.out.println("It is a palindrome.");
+                } else {
+                    System.out.println("It is not a palindrome.");
+                }
             }
-            left++;
-            right--;
         }
 
-
-        System.out.println("String: " + input);
-        if (isPalindrome) {
-            System.out.println("It is a palindrome.");
-        } else {
-            System.out.println("It is not a palindrome.");
-        }
     }
-}
+
