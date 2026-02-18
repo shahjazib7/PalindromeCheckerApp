8ac11e88775cc8d7ecff4b8ceb17a37184040752
shahjazib7 <sz9838@srmist.edu.in>
2026-02-18 09:44:52 +0530

    UC1

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -2,6 +2,30 @@ import java.util.Scanner;
 
 public class PalindromeChecker {
     public static void main(String[] args) {
-        System.out.println("Welcome Palindrome");
+
+
+        String input = "madam";
+        String str = input.toLowerCase();
+
+        int left = 0;
+        int right = str.length() - 1;
+        boolean isPalindrome = true;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+
+        System.out.println("String: " + input);
+        if (isPalindrome) {
+            System.out.println("It is a palindrome.");
+        } else {
+            System.out.println("It is not a palindrome.");
+        }
     }
 }
