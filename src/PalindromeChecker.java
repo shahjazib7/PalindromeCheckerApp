18e81a0f70d093280856b88fe03f7d1982e19c82
shahjazib7 <sz9838@srmist.edu.in>
2026-02-25 09:49:11 +0530

    UC11

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,7 +1,34 @@
 import java.util.Scanner;
 
-public class PalindromeChecker {
+class PalindromeIgnore {
+
+    static boolean isPalindrome(String str) {
+
+        // Remove spaces and convert to lowercase
+        str = str.replaceAll("\\s", "").toLowerCase();
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right))
+                return false;
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome Palindrome");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        if (isPalindrome(input))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
     }
 }
